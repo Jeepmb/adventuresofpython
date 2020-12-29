abe141373fb70211ab07a82185f515e0d893e777
Cody Hutchins <cody.hutchins@darkwolfsolutions.com>
2020-12-29 12:05:30 -0700

    quick refactor for clarity

--- matrix_test/objects.py
+++ matrix_test/objects.py
@@ -9,7 +9,11 @@ class Pub:
 def main():
 	bars = []
 	for i in range(100):
-		bars.append(Pub("Pub No."+str(randint(0,100)),randint(0,12),randint(13,23)))
+		pubName="Pub No."+str(randint(0,100))
+		pubOpenTime=randint(0,12)
+		pubCloseTime=randint(13,23) # Using military time because why not
+		thisPub=Pub(pubName,pubOpenTime,pubCloseTime)
+		bars.append(thisPub)
 
 	for bar in bars:
 		print("Bar:",bar.name," opens at ",bar.openTime," and closes at ",bar.closeTime)
