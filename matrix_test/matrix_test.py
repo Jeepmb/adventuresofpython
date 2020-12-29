334f0597af28a241fd1690a3478bccfa335e7de9
Jeepmb <tugg.hutchins@gmail.com>
2020-12-28 19:25:26 -0800

    Update matrix_test.py
    Added error handling for bar names that aren't in the database (future database to be exact)

--- matrix_test/matrix_test.py
+++ matrix_test/matrix_test.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
-usr_choice = input("What bar would you like to know about?:")
+usr_choice = input("What bar would you like to know about?:\n")
 
 bars = [
         ["Manito Taphouse","3","6","All",],
         ["Republic Pi","4","6","Monday"]
         ]
+
+end = ""
 
 for element in bars:
     for sub_element in element:
@@ -13,5 +15,7 @@ for element in bars:
             print(sub_element)
             start = element[1]
             end = element[2]
+            print("The happy hour for " + usr_choice + " is from " + start + " to " + end)
 
-print("The happy hour for " + usr_choice + " is from " + start + " to " + end)
+if end == "":
+    print("That bar is not yet listed in our database")
