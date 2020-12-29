b261755b758136c01f6eb000fe0ca3c18a3ce09b
Jeepmb <tugg.hutchins@gmail.com>
2020-12-28 19:03:53 -0800

    Update matrix_test.py
    Learned how to get the "for" function to search the respective lists and return the proper times. Now I need to work on error handling.

--- matrix_test/matrix_test.py
+++ matrix_test/matrix_test.py
@@ -3,21 +3,15 @@
 usr_choice = input("What bar would you like to know about?:")
 
 bars = [
-        ["Manito",3,6,"All",],
-        ["Republic",4,6,"Monday"]
+        ["Manito Taphouse","3","6","All",],
+        ["Republic Pi","4","6","Monday"]
         ]
 
-pos = bars.index(usr_choice)   #BROKEN - list.index() can't seem to search through a matrix (2D Array), ony works in lists.
+for element in bars:
+    for sub_element in element:
+        if usr_choice in sub_element:
+            print(sub_element)
+            start = element[1]
+            end = element[2]
 
-# Insert some code here to convert the (x,y) output from "pos" to get just the "Y" position (or row)
-
-y_pos = ""
-
-start_time = bars[y_pos][1]
-end_time = bars[y_pos][2]
-
-if usr_choice in bars:
-    print(usr_choice + " is open for happy hour from " + start_time + " to " + end_time)
-else:
-    print("Not located in database!")
-
+print("The happy hour for " + usr_choice + " is from " + start + " to " + end)
